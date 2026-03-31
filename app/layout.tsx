41e07527afe394d9368147c3bc653066968d9247
Hermes (agora) <hermes.frangoudis@agora.io>
2026-03-31 15:03:17 -0400

    ui adjustments

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={instrumentSans.variable}>
+    <html lang="en" className={`${instrumentSans.variable} dark`}>
       <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
